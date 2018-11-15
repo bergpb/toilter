118cbc121d3c6034078c97485e8ef7bb2a0617f0
bergpb <lindembergdepaulo@gmail.com>
2018-11-15 13:54:48 -0300

    Aula 8 - Templates (Continuação)

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -3,8 +3,12 @@ from flask import render_template
 from app import app
 
 
-@app.route("/index/<user>")
-@app.route("/", defaults={'user':None})
-def index(user):
-    return render_template('index.html',
-                           user=user)
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('base.html')
